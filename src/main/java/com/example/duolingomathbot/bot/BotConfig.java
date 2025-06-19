baf4e2dc5b5c420f368464784841579a5d8d4bcd
alexgnedash <asgnedash@gmail.com>
2025-06-19 15:44:52 -0300

    1

--- src/main/java/com/example/duolingomathbot/bot/BotConfig.java
+++ src/main/java/com/example/duolingomathbot/bot/BotConfig.java
@@ -1,10 +1,22 @@
 package com.example.duolingomathbot.bot;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
 public class BotConfig {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(MathSrTelegramBot bot) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(bot);
+        return telegramBotsApi;
+    }
+
 
     @Value("${telegram.bot.username}")
     private String botUsername;
