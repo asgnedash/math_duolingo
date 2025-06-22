10b7cf7a0afd61f2284ea4fd2e1f6a65a3180d62
asgnedash <145232460+asgnedash@users.noreply.github.com>
2025-06-22 00:06:18 -0300

    Handle Google Sheets errors in MarathonService

--- src/main/java/com/example/duolingomathbot/repository/UserRepository.java
+++ src/main/java/com/example/duolingomathbot/repository/UserRepository.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByTelegramId(Long telegramId);
+
+    java.util.List<User> findByMarathonTrue();
 }
